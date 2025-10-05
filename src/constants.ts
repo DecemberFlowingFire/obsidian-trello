ae83072b301a12fd19c73feb5afb88381fcb534e
Chris Chinchilla <chris@chrischinchilla.com>
2025-10-05 13:34:08 +0200

    Build: release 2.3.3

--- src/constants.ts
+++ src/constants.ts
@@ -51,7 +51,7 @@ export const DEFAULT_SETTINGS: PluginSettings = {
 
 export const DEFAULT_DATA: PluginData = {
   settings: DEFAULT_SETTINGS,
-  version: '2.3.2',
+  version: '2.3.3',
   firstRun: true,
   connectedCards: {}
 };
